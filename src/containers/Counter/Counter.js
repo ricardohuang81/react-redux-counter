610184e1d36e4f9ab11f2507f79b62698e7792b4
Ricky Huang <huangricky@gmail.com>
2019-03-18 16:53:10 -0400

    added Redux Thunk to handle asynchronous code using actionCreators

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+import * as actionCreators from '../../store/actions/index';
 import './Counter.css';
 
 class Counter extends Component {
@@ -36,12 +36,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
-        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
-        onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 4 }),
-        onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, val: 8 }),
-        onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result: result }),
-        onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElementId: id })
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
+        onAddCounter: () => dispatch(actionCreators.add(4)),
+        onSubtractCounter: () => dispatch(actionCreators.subtract(8)),
+        onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
     }
 }
 
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,25 +1,33 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(
+      result => result.id !== action.resultElementId
+    );
+    return updateObject(state, { results: updatedArray });
+}
+
 const resultReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: Date.now(), value: action.result })
-            }
+            // Change data
+            return updateObject(state, {
+              results: state.results.concat({
+                id: Date.now(),
+                value: action.result * 2
+              })
+            });
         case actionTypes.DELETE_RESULT:
             // const id = 3;
             // const newArray = [...state.results];
             // newArray.splice(id, 1);
-            const updatedArray = state.results.filter(result => result.id !== action.resultElementId)
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return deleteResult(state, action);
+            
         default:
             return state;
     }
